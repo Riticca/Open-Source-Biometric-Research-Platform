0f1a4f64144068a1c952f4baae61075a124ca05b
AsuWVikram <vikram.wathodkar@asu.edu>
2017-10-05 16:59:24 -0700

    Added buffer to read data and introduced sleep in threads

--- Biometric/Biometric/src/biometricgui/GraphPlotter.java
+++ Biometric/Biometric/src/biometricgui/GraphPlotter.java
@@ -5,35 +5,108 @@
  */
 package biometricgui;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.data.general.SeriesException;
+import org.jfree.data.time.Second;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
 
 /**
- *
- * @author vikram
+ * @author Vikram Wathodkar (vikram.wathodkar@gmail.com)
+ * This class read the given file and plot graph on panels
  */
 public class GraphPlotter implements Runnable {
 
+    /**
+     * Initialize required stuff
+     * @param panel the place where to draw graph
+     * @param filePath absolute path of file to be read
+     */
     public GraphPlotter(javax.swing.JPanel panel, String filePath) {
         
-        PlotTheGraphs gr = new PlotTheGraphs();
-        final XYDataset dataset = gr.series(filePath);
-        final JFreeChart chart = ChartFactory.createTimeSeriesChart("EMG Readings","Seconds","EMG value captured",dataset,false,false,false);
-        final ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setSize(panel.getSize());
-        panel.add(chartPanel);
-        panel.revalidate();
-        panel.repaint();
+        this.panel = panel;
+        series = new TimeSeries("Biometric Data");
+        current = new Second( );
+        
+        /* We already made sure that file exists
+         * but my IDE wants me to double check it
+         */
+        try {
+            fileReader = new BufferedReader(new FileReader(filePath));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(GraphPlotter.class.getName()).log(
+                    Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void run() {
-        System.out.println("New thread is created");
+        
+        Double biometricValue;
+        int endOfFileFlag = 0;
+        
+        while( true )
+        {
+            series.clear();
+            
+            /* Let's plot 50 values at a time */
+            for (int i = 0; i < 50; i++) 
+                
+                try {
+                   biometricValue = new Double(fileReader.readLine());
+                   series.add(current, biometricValue);
+                   current = ( Second ) current.next( ); // TODO: it will come
+                                                         // from the file
+                } catch ( SeriesException e ) {
+                    System.err.println("Error adding to series");
+                } catch ( IOException ex ) {
+                    /* Everything that has a beginning, has an ending */
+                    endOfFileFlag = 1;
+                    break;
+                }
+            
+            /* Exit on EOF */
+            if (endOfFileFlag == 1)
+                break;
+            
+            /* Draw the graph */
+            dataset = new TimeSeriesCollection(series);
+            chart = ChartFactory.createTimeSeriesChart("EMG Readings",
+                    "Seconds", "EMG value captured", dataset,
+                    false, false, false);
+            
+            chartPanel = new ChartPanel(chart);
+            chartPanel.setSize(panel.getSize());
+            chartPanel.setRefreshBuffer(true);
+            panel.add(chartPanel);
+            panel.revalidate();
+            panel.repaint();
+            
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(GraphPlotter.class.getName()).log(
+                        Level.SEVERE, null, ex);
+            }
+        }
+        
     }
     
-    
+    private TimeSeries series;
+    private Second current;
+    private BufferedReader fileReader;
+    private XYDataset dataset;
+    private JFreeChart chart;
+    private javax.swing.JPanel panel;
+    private ChartPanel chartPanel;
+
 }
